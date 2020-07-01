e77dc8d3ed86059be3ac241cc7292380b5fa6034
Estevan Maito <ejmaito@gmail.com>
2020-07-01 16:02:15 -0300

    chore(cta): update link to github project

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -4,7 +4,7 @@ function CTA() {
   return (
     <a
       className="flex items-center justify-between p-4 mb-8 text-sm font-semibold text-purple-100 bg-purple-600 rounded-lg shadow-md focus:outline-none focus:shadow-outline-purple"
-      href="https://github.com/estevanmaito/windmill-dashboard"
+      href="https://github.com/estevanmaito/windmill-dashboard-react"
     >
       <div className="flex items-center">
         <svg className="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 20 20">
