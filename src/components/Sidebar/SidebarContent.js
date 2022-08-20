82efb4c64632b1fd339043d9f5e8cd01b9d1ddfd
Lázaro Amor Sevilla <lazaroesunagranpersona@gmail.com>
2022-08-20 17:05:32 +0200

    Fix sidebar active links

--- src/components/Sidebar/SidebarContent.js
+++ src/components/Sidebar/SidebarContent.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import routes from '../../routes/sidebar'
-import { NavLink, Route } from 'react-router-dom'
+import { NavLink, Route, Routes } from 'react-router-dom'
 import * as Icons from '../../icons'
 import SidebarSubmenu from './SidebarSubmenu'
 import { Button } from '@windmill/react-ui'
@@ -11,6 +11,10 @@ function Icon({ icon, ...props }) {
 }
 
 function SidebarContent() {
+  
+  let className = 'inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200';
+  let activeClassName = className + ' text-gray-800 dark:text-gray-100'
+
   return (
     <div className="py-4 text-gray-500 dark:text-gray-400">
       <a className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
@@ -25,15 +29,17 @@ function SidebarContent() {
               <NavLink
                 exact
                 to={route.path}
-                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                activeClassName="text-gray-800 dark:text-gray-100"
+                className={({ isActive }) => 
+                  isActive ? activeClassName : className
+                }
               >
-                <Route path={route.path} exact={route.exact}>
-                  <span
-                    className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-                    aria-hidden="true"
-                  ></span>
-                </Route>
+                <Routes>
+                  <Route path={route.path} exact={route.exact} element={<span
+                      className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                      aria-hidden="true"
+                    ></span>}>
+                  </Route>
+                </Routes>
                 <Icon className="w-5 h-5" aria-hidden="true" icon={route.icon} />
                 <span className="ml-4">{route.name}</span>
               </NavLink>
